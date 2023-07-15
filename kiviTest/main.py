c86170e5839ee389a0efa9d2fdf0f09d451202d7
EyalKeysar <eyal@some.com>
2023-07-15 17:57:14 +0300

    pivoting kivy

--- kiviTest/main.py
+++ kiviTest/main.py
@@ -2,12 +2,19 @@ import kivy
 from kivy.app import App
 # from kivy.uix.label import Label
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
+from kivy.config import Config
+Config.set('graphics', 'resizable', False)
 
 kivy.require('2.2.1') 
 
 class MainGrid(Widget):
-    pass
+    name = ObjectProperty(None)
+
+    def btn(self):
+        print(f'Hello {self.name.text}')
+        self.name.text = ""
 
 class MainApp(App):
     def build(self):
